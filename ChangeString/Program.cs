c33b648e79869cd9b63b4d237cb26f67b7ed346c
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-06 19:27:15 +0000

    Request user a character

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,15 +6,17 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
+            //ask the user for a string
             Console.Write("String: ");
             string s = Console.ReadLine();
 
+
+            //ask the user for a character
             Console.Write("Caráter: ");
             string character = Console.ReadLine();
             char c = char.Parse(character);
 
 
-            Console.WriteLine("Hello, World!");
         }
     }
 }
