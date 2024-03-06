a00e1e69e33400ee16a94ce0a68aa9268d64d5af
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-06 18:42:37 +0000

    Added for each in args

---
+++ RightSize/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            foreach (string s in args)
+            {
+                Console.WriteLine(s);
+            }
+                
+        }
+    }
+}
