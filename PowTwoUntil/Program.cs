eb4fbf9cfc416a59e8eb091a02ff77be2da51797
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-07 22:01:09 +0000

    Created the method of power of 2

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        /// <summary>
+        /// It will print power of 2 of numbers 1,2,4,8,16
+        /// </summary>
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (1<<5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
